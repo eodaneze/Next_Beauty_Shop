f90350798d54308a7bdb6f4efc9e760d0e5e4ade
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-30 05:36:55 -0800

    designing beautyservice cooperate footer and footercondition section

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,9 @@ import Discover from '@/components/Discover'
 import Deals from '@/components/Deals'
 import ValentineDaySection from '@/components/ValentineDaySection'
 import RewardSection from '@/components/RewardSection'
+import FooterSection from '@/components/FooterSection'
+import CooperateSection from '@/components/CooperateSection'
+import BeautyServiceSection from '@/components/BeautyServiceSection'
 
 
 
@@ -24,6 +27,9 @@ export default function Home() {
        <Deals />
        <ValentineDaySection />
        <RewardSection />
+       <BeautyServiceSection />
+       <CooperateSection />
+       <FooterSection />
     </>
   )
 }
